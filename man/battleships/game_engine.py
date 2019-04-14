b0c951f0e4c578633dcc16c5b628c5a25ee3e6fa
Enis <e.nazif@warwick.ac.uk>
2019-04-14 23:03:17 +0100

    clean up game_engine

--- man/battleships/game_engine.py
+++ man/battleships/game_engine.py
@@ -1,6 +1,6 @@
 from tqdm import tqdm
 from man.battleships.types.Game import Game
-from man.battleships.config import BOARD_SIZE, GAMES_PER_MATCH
+from man.battleships.config import GAMES_PER_MATCH
 
 
 # TODO: should a player own its own board?
@@ -9,15 +9,17 @@ from man.battleships.config import BOARD_SIZE, GAMES_PER_MATCH
 # TODO: Handle max move time
 
 
-def play_match(player_1_bot, player_2_bot, n_games=GAMES_PER_MATCH):
-    """ Plays 'n_games' between 'player_1_bot' and 'player_2_bot' and returns a dictionary of all game info"""
+def play_match(player_1_bot: str, player_2_bot: str, n_games=GAMES_PER_MATCH):
+    """
+    Plays 'n_games' between 'player_1_bot' and 'player_2_bot' and returns a dictionary of all game info
+
+    :param player_1_bot:
+    :param player_2_bot:
+    :param n_games:
+    :return:
+    """
 
     pb_desc = f"Playing {n_games} games between {player_1_bot} and {player_2_bot}"
-
-    game_data = []
-
-    for game_id in tqdm(range(n_games), desc=pb_desc):
-        g = Game(player_1_bot, player_1_bot, game_id)
-        game_data.append(g.play_game())
+    game_data = [Game(player_1_bot, player_1_bot, game_id).play_game() for game_id in tqdm(range(n_games), desc=pb_desc)]
 
     return game_data
