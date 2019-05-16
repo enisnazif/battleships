eac56500deb6bfd844c0601d4b0498f73dec33b3
egao1980 <egao1980@gmail.com>
2019-05-16 13:49:09 +0100

    Placement strategy

--- bots/MegaBot.py
+++ bots/MegaBot.py
@@ -12,14 +12,16 @@ class MegaBot(Bot):
         super().__init__()
         self.my_shots = dict()
         self.board = [[BOARD_SIZE] * BOARD_SIZE] * BOARD_SIZE
-        self.max_xs = [BOARD_SIZE] * BOARD_SIZE
-        self.max_ys = [BOARD_SIZE] * BOARD_SIZE
+        self.hit_xs = [BOARD_SIZE] * BOARD_SIZE
+        self.hit_ys = [BOARD_SIZE] * BOARD_SIZE
         self.hunt_mode = False
         self.hunted = set()
 
-    def get_ship_placements(self, ships: List[Ship]) -> List[Tuple[Ship, Point, Orientation]]:
+    def get_ship_placements(self, ships: List[Ship]) -> List[Tuple[Ship,
+                                                                   Point, Orientation]]:
         """
-        Returns a set of point at which a ship will be placed. For each ship, you must return a tuple of [Ship, Point, Orientation],
+        Returns a set of point at which a ship will be placed. For each
+ship, you must return a tuple of [Ship, Point, Orientation],
         where Point corresponds to the bottom left corner of the Ship, e.g:
 
                      -
@@ -27,27 +29,25 @@ class MegaBot(Bot):
                      -
                      x
 
-        and Orientation is one of Orientation.Vertical and Orientation.Horizontal
+        and Orientation is one of Orientation.Vertical and
+Orientation.Horizontal
 
-        :param ships: A List of Ship which your bot should return placements for
+        :param ships: A List of Ship which your bot should return
+placements for
         :return: A list of placements for each ship in 'ships'
         """
+        PLACEMENTS = [
+            [
+                (ships[0], Point(1, 1), Orientation.Horizontal),
+                (ships[1], Point(7, 2), Orientation.Vertical),
+                (ships[2], Point(6, 8), Orientation.Horizontal),
+                (ships[3], Point(2, 7), Orientation.Horizontal),
+                (ships[4], Point(1, 4), Orientation.Horizontal),
+            ],
+        ]
 
-        # Perform random ship placement
-        while True:
-            placements = []
-            for ship in ships:
-                random_orientation = random.choice(list(Orientation))
-                random_point = Point(
-                    random.randrange(0, BOARD_SIZE), random.randrange(0, BOARD_SIZE)
-                )
-
-                placements.append((ship, random_point, random_orientation))
-
-            if Board.is_valid_ship_placement(placements):
-                break
-
-        return placements
+        ships = sorted(ships, key=lambda x: len(x), reverse=True)
+        return random.choice(PLACEMENTS)
 
     @staticmethod
     def vicinity(p):
@@ -78,18 +78,36 @@ class MegaBot(Bot):
         last_point = last_shot.get('shot')
         error = last_shot.get('error')
         if not error:
+            if last_point:
+                x = last_point.x
+                y = last_point.y
+                mx, my = 0, 0
+                for i in range(BOARD_SIZE):
+                    if Point(i, y) in self.my_shots:
+                        mx += 1
+                    if Point(x, i) in self.my_shots:
+                        my += 1
+
+                self.hit_xs[x] = mx
+                self.hit_ys[y] = my
+
             self.my_shots[last_point] = last_shot
+            is_sunk = last_shot.get('is_sunk', False)
+            is_hit = last_shot.get('is_hit', False)
 
-        is_sunk = last_shot.get('is_sunk', False)
-        is_hit = last_shot.get('is_hit', False)
-
-        if is_hit and not is_sunk:
-            self.hunt_mode = True
-            self.hunted = self.hunted.union(self.vicinity(last_point)) - set(self.my_shots.keys())
-        elif is_sunk:
-            self.hunt_mode = False
+            if is_hit and not is_sunk:
+                self.hunt_mode = True
+                self.hunted = self.hunted.union(self.vicinity(last_point)) - set(self.my_shots.keys())
+            elif is_sunk:
+                self.hunt_mode = False
 
         if not self.hunt_mode:
+            min_x = min(self.hit_xs)
+            min_y = min(self.hit_ys)
+
+            ix = [i for i, v in enumerate(self.hit_xs) if v == min_x]
+            iy = [i for i, v in enumerate(self.hit_ys) if v == min_y]
+
             x = random.randrange(0, BOARD_SIZE)
             y = random.randrange(0, BOARD_SIZE)
 
